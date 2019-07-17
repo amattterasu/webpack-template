604054c3ed2c883cf0812591f391279e7e015fd2
Andrey Gubaydullin <amattterasuuu@gmail.com>
2019-07-17 15:39:06 +0600

    added css and scss loaders, configured the config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,49 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const conf = {
-    devtool: 'source-map',
-    entry: { 
-        app:'./src/index.js' 
+    entry: {
+        app: './src/index.js'
     },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].js',
         publicPath: 'dist/'
     },
-    
     devServer: {
         overlay: true
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+                filename: '[name].css',
+            })
+    ]
 };
 
 module.exports = conf;
