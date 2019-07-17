b2e93f3ae40de510fbc71c33f7ccf9f2e5651f6a
Andrey Gubaydullin <amattterasuuu@gmail.com>
2019-07-17 22:27:39 +0600

    added build with configs

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/'
+}
+
+const conf = {
+
+    externals: {
+        paths: PATHS
+    },
+    entry: {
+        app: PATHS.src
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].css`,
+        }),
+        new HtmlWebpackPlugin({
+            template: `${PATHS.src}/index.html`,
+            filename: './index.html',
+        }),
+        new CopyWebpackPlugin([
+            { from: `${PATHS.src}/${PATHS.assets}/img`, to: `${PATHS.assets}/img` },
+            { from: `${PATHS.src}/${PATHS.assets}/fonts`, to: `${PATHS.assets}/fonts` },
+            { from: `${PATHS.src}/static`, to: '' }
+        ])
+    ]
+};
+
+module.exports = conf;
